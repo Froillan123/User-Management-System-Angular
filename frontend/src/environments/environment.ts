950151be721f55c6d39b22646909430c4a1c0fe7
Jane Lopez <139733763+Lopezjane@users.noreply.github.com>
2025-05-20 10:51:15 +0800

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
     production: false,
     apiUrl: isLocalhost 
         ? 'http://localhost:4000/accounts'
-        : 'https://user-management-system-angular-laygan.onrender.com/accounts',
+        : 'https://user-management-system-angular-laygan-bu8d.onrender.com/accounts',
     wsUrl: isLocalhost
         ? 'ws://localhost:4000'
-        : 'wss://user-management-system-angular-laygan.onrender.com'
+        : 'wss://user-management-system-angular-laygan-bu8d.onrender.com'
 };
 
